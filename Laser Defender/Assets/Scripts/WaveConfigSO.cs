958a63eb6e715cff4db13a7a92a167a3c9065b10
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-10-30 00:57:11 -0400

    6.8 Instantiating Enemies
    Created a script called EnemySpawner that will handle instantiating enemies on a spawn timer.

--- Laser Defender/Assets/Scripts/WaveConfigSO.cs
+++ Laser Defender/Assets/Scripts/WaveConfigSO.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
 
     public Transform GetStartingWaypoint()
     {
