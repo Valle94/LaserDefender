db6fd5bfff85977e6927db9846860a34cf241bb9
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-11-15 23:04:44 -0500

    6.26 Singleton Scorekeeper
    Enabled another singleton pattern to handle our scorekeeper which shows our final score on the 'Game Over' screen.

--- Laser Defender/Assets/Scripts/LevelManager.cs
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] float sceneLoadDelay = 2f;
+    ScoreKeeper scoreKeeper;
+
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
     public void LoadGame()
     {
         SceneManager.LoadScene("Game");
@@ -13,6 +20,7 @@ public class LevelManager : MonoBehaviour
 
     public void LoadMainMenu()
     {
+        scoreKeeper.ResetScore();
         SceneManager.LoadScene("MainMenu");
     }
 
--- Laser Defender/Assets/Scripts/ScoreKeeper.cs
+++ Laser Defender/Assets/Scripts/ScoreKeeper.cs
@@ -6,6 +6,26 @@ public class ScoreKeeper : MonoBehaviour
 {
     int initialScore = 0;
     int score;
+    static ScoreKeeper instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public int GetScore()
     {
