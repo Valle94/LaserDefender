002d49e7276bc2cab814a866fb8eebee3c2cf54f
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-11-15 20:02:00 -0500

    6.18 Sound Effect
    Imported sound effects and added sounds for laser firing and taking damage.

--- Laser Defender/Assets/Scripts/Shooter.cs
+++ Laser Defender/Assets/Scripts/Shooter.cs
@@ -19,6 +19,12 @@ public class Shooter : MonoBehaviour
     [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
+    AudioPlayer audioPlayer;
+
+    void Awake()
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
 
     void Start()
     {
@@ -65,6 +71,8 @@ public class Shooter : MonoBehaviour
             float timeToNextProjectile = UnityEngine.Random.Range(baseFiringRate - firingRateVariance,
                                                                 baseFiringRate + firingRateVariance);
             timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+            audioPlayer.PlayShootingClip();
 
             yield return new WaitForSeconds(timeToNextProjectile);
         }
